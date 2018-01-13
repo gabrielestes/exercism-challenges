ea35cb6782d49742d96ddd2f30b28a9f6ba42e65
Gabriel Estes <gabriel.estes@mckesson.com>
2018-01-13 17:40:56 -0500

    refactor and use unpacking

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,6 +1,4 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("The supplied strands are not of equal length.")
-    strand_pairs = zip(strand_a, strand_b)
-    distance = len([pair for pair in strand_pairs if pair[0] != pair[1]])
-    return distance
+    return len([(x,y) for x,y in zip(strand_a, strand_b) if x != y])

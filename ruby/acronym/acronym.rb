f3f7f68827c885a82793fde0da9409aa36a7cb62
gabrielestes <gabriel.estes@mckesson.com>
2019-03-22 18:39:38 -0400

    second solution to acronym

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,8 +1,8 @@
 class Acronym
-	# Converts a string to an acronym.
-	# @param [String]
-	# @return [String] representing acronym of input
-	def self.abbreviate(input)
-		input.scan(/\w+/).map(&:chr).join.upcase
-	end
+  # Converts a string to an acronym.
+  # @param [String]
+  # @return [String] representing acronym of input
+  def self.abbreviate(input)
+    input.scan(/\b[a-zA-Z]/).join.upcase
+  end
 end

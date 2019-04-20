f3655a91fc2d4dc3d92bfd968dffbb19a76db2cf
gabrielestes <gabriel.estes@mckesson.com>
2019-04-19 23:05:03 -0400

    solve hamming problem in ruby

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,19 +1,12 @@
 class Hamming
-  def initialize
-  end
+  def self.compute(nuc_one, nuc_two)
+    raise ArgumentError if nuc_one.length != nuc_two.length
 
-  def compute(nuc_one, nuc_two)
-    unless nuc_one.length == nuc_two.length
-      raise ArgumentError
-    end
-    nuc_one = nuc_one.scan(/\w/)
-    nuc_two = nuc_two.scan(/\w/)
-    diff = 0
-    count = 0
-    nuc_one.each do |gene|
-      diff += 1 if nuc_two[count] != gene
-      count += 1
-    end
-    diff
+    nucleotide_pairs = nuc_one.split('').zip(nuc_two.split(''))
+    nucleotide_pairs.reject { |x, y| x == y }.count
   end
+end
+
+module BookKeeping
+  VERSION = 3
 end

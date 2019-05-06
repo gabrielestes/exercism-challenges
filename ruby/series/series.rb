41b3fea43d6e7cd9e5e9952363a51975c232305a
gabrielestes <gabriel.estes@mckesson.com>
2019-05-06 09:55:20 -0400

    complete the ruby series exercise

---
+++ ruby/series/series.rb
@@ -0,0 +1,15 @@
+class Series
+  def initialize(raw_number)
+    @number_series = raw_number.split('')
+  end
+
+  def slices(slice_length)
+    raise ArgumentError if slice_length > number_series.length
+
+    number_series.each_cons(slice_length).map(&:join)
+  end
+
+  private
+
+  attr_reader :number_series
+end

dff8120bd9d0d81cce95c8c71aa8f5dae298998c
gabrielestes <gabriel.estes@mckesson.com>
2019-05-27 15:18:41 -0400

    refactor leap solution

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,11 +1,5 @@
 class Year
   def self.leap?(year)
-    if (year % 400).zero?
-      true
-    elsif (year % 100).zero?
-      false
-    elsif (year % 4).zero?
-      true
-    end
+    (year % 4).zero? && !(year % 100).zero? || (year % 400).zero?
   end
 end
